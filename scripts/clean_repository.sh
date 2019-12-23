adfb644a28727673b65f557c9575d6e29cf3ce62
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2019-12-23 15:47:44 -0800

    Clean Repo Updated to remove every build artifacts

--- scripts/clean_repository.sh
+++ scripts/clean_repository.sh
@@ -3,17 +3,19 @@
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${BASEDIR}/../
 
+rm -rf artifacts/
 rm -rf build/
+rm -rf dist/
 rm -rf docs/_build/
 
-rm -rf nvtx_plugins/python/*.egg-info/
+rm -rf nvtx_plugins/**/*.egg-info/
 
-for i in {1..8}
+for i in {1..10}
 do
    rm -rf "$(seq -s**/ $i|tr -d '[:digit:]')*.so"
    rm -rf "$(seq -s**/ $i|tr -d '[:digit:]')__pycache__/"
 done
 
-find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
+find . | grep -E "(__pycache__|\.pyc|\.pyo|\.so$)" | xargs rm -rf
 
 rm -f examples/*qd*
