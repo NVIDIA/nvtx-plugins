eabb6f60bf704eaf57e592d8935d3de4a20a379b
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2020-01-17 11:13:06 -0800

    Scripts edit

--- examples/run_tf_session.sh
+++ examples/run_tf_session.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export CUDA_LAUNCH_BLOCKING=1
-nsys profile -d 60 -w true --sample=cpu -t 'nvtx,cuda' -o examples/tf_session_example python examples/tf_session_example.py
+nsys profile -d 60 -w true --force-overwrite=true --sample=cpu -t 'nvtx,cuda' --stop-on-exit=true --kill=sigkill -o examples/tf_session_example python examples/tf_session_example.py
--- scripts/docker/launch_NGC_TF.sh
+++ scripts/docker/launch_NGC_TF.sh
@@ -3,5 +3,6 @@
 docker run -it --rm \
   --runtime=nvidia \
   --ipc='host' \
-  -v $PWD:/workspace/nvtx \
-  nvcr.io/nvidia/tensorflow:19.11-tf1-py3 /bin/bash
+  -v "$(pwd):/workspace/nvtx" \
+  --workdir /workspace/nvtx/ \
+  nvcr.io/nvidia/tensorflow:19.12-tf1-py3 /bin/bash
