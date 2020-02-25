5af7c1c4bf33f94b6df6dff1ec070ace17e18f51
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2020-02-24 18:13:09 -0800

    Docs Updated

--- docs/conf.py
+++ docs/conf.py
@@ -102,7 +102,7 @@ language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['requirements.txt']
+exclude_patterns = ['requirements.txt', 'venv/*']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 # default_role = None
